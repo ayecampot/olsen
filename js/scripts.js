da892153e032352bfb90ddaf25f7362d384d51f0
ayecampot <ayecampot@gmail.com>
2022-12-22 13:25:49 -0300

    saco scroll horizontal mobile

--- js/scripts.js
+++ js/scripts.js
@@ -27,9 +27,7 @@ window.addEventListener('DOMContentLoaded', event => {
 
     // Shrink the navbar 
     navbarShrink();
- 
 
-    
 
     // Shrink the navbar when page is scrolled
     document.addEventListener('scroll', navbarShrink);
