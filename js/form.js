05e0f300e3c8446b3eecfb8c004ca2e2c7ecf6b5
ayecampot <ayecampot@gmail.com>
2023-02-07 14:09:36 -0300

    cambio inputs

--- js/form.js
+++ js/form.js
@@ -19,12 +19,12 @@ async function handleSubmit(event) {
 		if (Object.hasOwn(data, 'errors')) {
 		  status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
 		} else {
-		  status.innerHTML = "Oops! There was a problem submitting your form"
+		  status.innerHTML = "Oops! Hubo un porblema al enviar tu formulario"
 		}
 	  })
 	}
   }).catch(error => {
-	status.innerHTML = "Oops! There was a problem submitting your form"
+	status.innerHTML = "Oops! Hubo un porblema al enviar tu formulario"
   });
 }
 form.addEventListener("submit", handleSubmit)
